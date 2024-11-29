4f49319066baa5099cd94f426a1c870aec3cfba3
Jorge Quintanar <jorge.quintanar2025@gmail.com>
2024-11-29 16:00:34 -0700

    Agregar archivos de proyecto.

---
+++ Jaaqcore/Controllers/ClienteController.cs
@@ -0,0 +1,56 @@
+﻿using Jaaqcore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jaaqcore.Controllers
+{
+    [ApiController]
+    [Route("Client")]
+    public class ClienteController : ControllerBase
+    {
+        //Localhost/client/ClientList
+        [HttpGet]
+        [Route("ClientList")]
+        public dynamic GetClients()
+        {
+            //
+            List<Client> Clients = new List<Client>
+            {
+                 new Jaaqcore.Models.Client
+                 {
+
+                    Id = "1",
+                    Name = "Jorge",
+                    Email = "jorge@gmail.com",
+                    Edad = "30"
+
+                  },
+                 new Jaaqcore.Models.Client
+                  {
+
+                    Id = "2",
+                    Name = "Jose",
+                    Email = "Jose@gmail.com",
+                    Edad = "29"
+
+                  },
+                 new Jaaqcore.Models.Client
+                  {
+
+                    Id = "3",
+                    Name = "Omar",
+                    Email = "Omar@gmail.com",
+                    Edad = "28"
+                  }
+};
+
+            return Clients;
+        }
+
+        //[HttpPost]
+        //[Route("AddClient")]
+        //public dynamic AddCliente()
+        //{
+
+        //}
+    }
+}
---
+++ Jaaqcore/Models/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace Jaaqcore.Models
+{
+    public class Client
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Edad { get; set; }
+        public string Email { get; set;}
+    }
+}
