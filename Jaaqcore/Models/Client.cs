be3b6a87721135b4bf64e56acdc77ef5b7df65e6
Jorge Quintanar <jorge.quintanar2025@gmail.com>
2024-12-02 13:53:15 -0700

    Model accept null

--- Jaaqcore/Models/Client.cs
+++ Jaaqcore/Models/Client.cs
@@ -1,10 +1,10 @@
 ﻿namespace Jaaqcore.Models
 {
-    public class Client
+    public class Client()
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Edad { get; set; }
-        public string Email { get; set;}
+        public string? Id { get; set; } 
+        public string? Name { get; set; }
+        public string? Edad { get; set; }
+        public string? Email { get; set;}
     }
 }
